5faadf5494c3e606d3e42448b94f44c31b7c1279
satishgupta07 <satishgupta.cse01@gmail.com>
2020-08-08 17:13:16 +0530

    Controlling the snake with arrow keys
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById("snake");
 var ctx = canvas.getContext("2d");
-console.log(canvas)
+// console.log(canvas)
 
 const grid = 32;
 //load img
@@ -22,6 +22,24 @@ let beer = {
     y : Math.floor(Math.random()*15+1) * grid
 }
 
+   let d;
+  document.addEventListener("keydown",(e)=>{
+    let k = e.keyCode
+    if(k==37){
+       d="left"
+    }
+    else if(k==38){
+        d="up"
+    }
+    else if(k==39){
+        d="right"
+    }
+    else if(k==40){
+        d="down"
+    }
+    console.log(d)
+})
+
 function draw(){
     ctx.drawImage(g,0,0)
 
@@ -33,6 +51,29 @@ function draw(){
     }
 
     ctx.drawImage(beerImage,beer.x,beer.y);
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(d=="left"){
+        snakeX -= grid;
+    }
+    if(d=="right"){
+        snakeX += grid;
+    }
+    if(d=="up"){
+        snakeY -= grid;
+    }
+    if(d=="down"){
+        snakeY += grid;
+    }
+
+    snake.pop();
+    let newpos = {
+        x : snakeX,
+        y : snakeY
+    }
+    snake.unshift(newpos)
 }
 
 var game = setInterval(draw,100);
